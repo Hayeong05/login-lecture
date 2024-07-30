4634e42bec67b88c30d1d08e764e6148439d4361
김하영 <0kingapple140@gmail.com>
2024-07-30 22:10:02 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,19 @@
 "use strict"
 
-console.log("hello");
-console.log("bye");
+const { login } = require("../../../routes/home/home.ctrl");
+
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+    console.log(id);
+    console.log("hello");
+
+    loginBtn.addEventListener("click", login);
+
+    function login() {
+        const req = {
+            id : id.value,
+            psword : psword.value,
+        };
+    }
